6e7db6771b7c6c62062fb3e4722276a11472f4b7
beet <zpharnoex@gmail.com>
2020-12-01 07:35:34 +0800

    fix: tb reloading

--- src/render/htmlWrapper.js
+++ src/render/htmlWrapper.js
@@ -82,6 +82,7 @@ export function renderHTML(body, pLink, pIdx) {
             { once: true }
           )
         }
+        Turbolinks.Location.prototype.isHTML = () => {return true}
         Turbolinks.start()
       </script>
     </body>
