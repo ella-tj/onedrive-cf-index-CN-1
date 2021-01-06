966352849c9a1e4fb56bd19cf91e51b79dc9428d
beet <63141491+beetcb@users.noreply.github.com>
2021-01-06 12:51:01 +0800

    fix: optimize pagination logic

--- src/render/htmlWrapper.js
+++ src/render/htmlWrapper.js
@@ -6,7 +6,7 @@ const COMMIT_HASH = '5d7579fcfb4729fcb855110c5f0ed5488d1d0d44'
 const pagination = (pIdx, attrs) => {
   const getAttrs = (c, h, isNext) =>
     `class="${c}" ${h ? `href="pagination?page=${h}"` : ''} ${
-      isNext === undefined ? '' : `onclick="handlePagination(${isNext})"`
+      isNext === undefined ? '' : `id=${c.includes('pre') ? 'pagination-pre' : 'pagination-next'}`
     }`
   if (pIdx) {
     switch (pIdx) {
@@ -25,9 +25,7 @@ const pagination = (pIdx, attrs) => {
 }
 
 export function renderHTML(body, pLink, pIdx) {
-  pLink = pLink || ''
   const p = 'window[pLinkId]'
-
   return `<!DOCTYPE html>
   <html lang="en">
     <head>
@@ -48,6 +46,12 @@ export function renderHTML(body, pLink, pIdx) {
       <script src="https://cdn.jsdelivr.net/gh/pipwerks/PDFObject/pdfobject.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/dplayer@1.26.0/dist/DPlayer.min.js"></script>
+      <style>
+        .paginate-container a {
+          cursor: pointer;
+          text-decoration: none;
+        }
+      </style>
     </head>
     <body>
       <nav id="navbar" data-turbolinks-permanent><div class="brand">☘️ Beet's OneDrive Index</div></nav>
@@ -64,28 +68,42 @@ export function renderHTML(body, pLink, pIdx) {
         }
         Prism.highlightAll()
         mediumZoom('[data-zoomable]')
-        if ('${pLink}') {
-          if (!window.pLinkId) history.pushState(history.state, '', location.pathname.replace('pagination', ''))
-          if (location.pathname.endsWith('/')) {
-            pLinkId = history.state.turbolinks.restorationIdentifier
-            ${p} = [['${pLink}'], 1]
-          }
-          if (${p}[0].length < ${p}[1]) (${p} = [[...${p}[0], '${pLink}'], ${p}[1]])
-        }
-        function handlePagination(isNext) {
-          isNext ? ${p}[1]++ : ${p}[1]--
-          addEventListener(
-            'turbolinks:request-start',
-            event => {
-              const xhr = event.data.xhr
-              xhr.setRequestHeader('pLink', ${p}[0][${p}[1] -2])
-              xhr.setRequestHeader('pIdx', ${p}[1] + '')
-            },
-            { once: true }
-          )
-        }
         Turbolinks.Location.prototype.isHTML = () => {return true}
         Turbolinks.start()
+        pagination()
+
+        function pagination() {
+          if ('${pLink ? 1 : ''}') {
+            if (location.pathname.endsWith('/')) {
+              pLinkId = history.state.turbolinks.restorationIdentifier
+              ${p} = { link: ['${pLink}'], idx: 1 }
+            } else if (!window.pLinkId) {
+              history.pushState(history.state, '', location.pathname.replace('pagination', '/'))
+              return
+            }
+            if (${p}.link.length < ${p}.idx) (${p} = { link: [...${p}.link, '${pLink}'], idx: ${p}.idx })
+          }
+          listen = ({ isNext }) => {
+            isNext ? ${p}.idx++ : ${p}.idx--
+            addEventListener(
+              'turbolinks:request-start',
+              event => {
+                const xhr = event.data.xhr
+                xhr.setRequestHeader('pLink', ${p}.link[${p}.idx -2])
+                xhr.setRequestHeader('pIdx', ${p}.idx + '')
+              },
+              { once: true }
+            )
+          }
+          preBtn = document.getElementById('pagination-pre')
+          nextBtn = document.getElementById('pagination-next')
+          if (nextBtn) {
+            nextBtn.addEventListener('click', () => listen({ isNext: true }), { once: true })
+          }
+          if (preBtn) {
+            preBtn.addEventListener('click', () => listen({ isNext: false }), { once: true })
+          }
+        }
       </script>
     </body>
   </html>`
